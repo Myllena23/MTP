920700e6b6dbf55e73f9632ec44e3c3d72cacfed
Myllena23 <myllenaof@gmail.com>
2017-09-05 11:53:45 -0300

    fim de aula

---
+++ aula20170905/seq1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+	int l,x=0;
+
+	printf("Digite o lado do quadrado: ");
+	scanf("%d",&l);
+	x=l*l;
+	printf("A area e %d\n",x);
+	
+	return 0;
+
+	
+}
---
+++ aula20170905/seq2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+	int b, h;
+
+	printf("Digite a base: ");
+	scanf("%d",&b);
+	printf("Digite a altura: ");
+	scanf("%d",&h);
+	printf("A area e %d",b*h);
+	return 0;
+}
