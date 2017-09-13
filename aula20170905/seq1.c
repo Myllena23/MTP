e30341ab55dc39063ef5193b0ab33efd12559d60
Myllena23 <31656016+Myllena23@users.noreply.github.com>
2017-09-13 18:22:24 -0300

    fim de aula

---
+++ aula20170905/seq1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+	int l,x=0;
+
+	printf("Digite o lado do quadrado: ");
+	scanf("%d",&l);
+	x=l*l;
+	printf("A area e %d\n",x);
+	
+	system ("pause");
+	return 0;
+
+}
