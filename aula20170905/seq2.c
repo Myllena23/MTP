687cd898a6f1c5ae36829763ec93dde122661454
Myllena23 <31656016+Myllena23@users.noreply.github.com>
2017-09-13 18:23:14 -0300

    fim de aula

---
+++ aula20170905/seq2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdlib.h>
+int main ()
+{
+	int b, h;
+
+	printf("Digite a base: ");
+	scanf("%d",&b);
+	printf("Digite a altura: ");
+	scanf("%d",&h);
+	printf("A area e %d",b*h \n);
+        system ("pause");
+	return 0;
+}
