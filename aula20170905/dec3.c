abc9f1d47c7781cefb93dd04623172e1786ccc1d
Myllena23 <31656016+Myllena23@users.noreply.github.com>
2017-09-13 21:23:36 -0300

    fim de aula

---
+++ aula20170905/dec3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main ()
+{
+	int a, b, c, s;
+
+	srand(time(0));
+	a = rand()%7;
+	b = rand()%7;
+	c = rand()%7;
+
+	s=a+b+c;
+
+	if (s==7||s==11)
+		printf ("\nDado 1:%i \nDado 2:%i \nDado 3:%i \nSoma:%i \nGanhou\n", a, b, c, s);
+	else printf ("\nDado 1:%i \nDado 2:%i \nDado 3:%i \nSoma:%i \nPerdeu\n", a, b, c, s);
+	
+	system ("pause");
+	return 0;
+}
